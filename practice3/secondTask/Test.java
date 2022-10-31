f337511f4f836ed37c4d59321b870ac570b65dbe
iraychev <92424190+iraychev@users.noreply.github.com>
2022-10-31 19:38:20 +0200

    tested more methods

--- practice3/secondTask/Test.java
+++ practice3/secondTask/Test.java
@@ -7,5 +7,7 @@ public class Test {
         Student gogo = new Student("gogo", "1.02.1999", "12364511", 5.10, 4.50, 3.00);
 
         gogo.examineTE();
+        gogo.examinePPE();
+        gogo.examinePIK();
     }
 }
